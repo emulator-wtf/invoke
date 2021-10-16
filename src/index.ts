d2b8b270165c6e15c353304183710b53c6fb2158
Madis Pink <madis.pink@gmail.com>
2021-10-17 00:43:26 +0300

    feat: add basic invoke logic

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { addPath, debug, exportVariable, getInput, setFailed, warning } from '@actions/core';
+import { exec } from '@actions/exec';
+
+const EW_CLI_URL = "https://maven.emulator.wtf/releases/ew-cli";
+
+async function invoke() {
+  try {
+    const token = getInput('api-token', { required: true });
+    const appApk = getInput('app-apk', { required: true });
+    const testApk = getInput('test-apk', { required: true });
+    const outputsDir = getInput('outputs-dir');
+
+    let args = ['--app', appApk, '--test', testApk];
+
+    if (outputsDir) {
+      args.push('--outputs-dir', outputsDir);
+    }
+
+    exec('ew-cli', args, { env: { EW_API_TOKEN: token } });
+  } catch (e) {
+    warning(`ew-cli invoke failed: ${e}`);
+    setFailed(e);
+  }
+}
+
+invoke();
